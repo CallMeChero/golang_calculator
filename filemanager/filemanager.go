5df06eecb94e6fc52e6477a9fc1ca827fdbbebeb
CallMeChero <david.ceranic@gmail.com>
2025-03-09 14:59:33 +0100

    Encapsulate logic into packages

---
+++ filemanager/filemanager.go
@@ -0,0 +1,34 @@
+package filemanager
+
+import (
+	"bufio"
+	"errors"
+	"os"
+)
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		file.Close()
+		return nil, errors.New("Failed to open file")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+
+	if err != nil {
+		file.Close()
+		return nil, errors.New("An error occured while reading file")
+	}
+
+	file.Close()
+	return lines, nil
+}
--- prices/prices.go
+++ prices/prices.go
@@ -1,10 +1,11 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strconv"
+
+	conversion "example.com/price-calculator/coversion"
+	filemanager "example.com/price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -14,44 +15,19 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
+
+	lines, err := filemanager.ReadLines("prices.txt")
 
 	if err != nil {
-		fmt.Println("An error occured!")
 		fmt.Println(err)
 		return
 	}
 
-	scanner := bufio.NewScanner(file)
-
-	var lines []string
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
+	prices, err := conversion.StringsToFloats(lines)
 
 	if err != nil {
-		fmt.Println("Erro while reading file")
 		fmt.Println(err)
-		file.Close()
 		return
-	}
-
-	prices := make([]float64, len(lines))
-
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-
-		if err != nil {
-			fmt.Println("Converting price to float failed")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-
-		prices[lineIndex] = floatPrice
 	}
 
 	job.InputPrices = prices
