05da948dacd3635201d28fbee01c67c59909973c
CallMeChero <david.ceranic@gmail.com>
2025-03-09 16:01:07 +0100

    Add swappable struct for console prints or creating json and improve error handling

--- main.go
+++ main.go
@@ -11,8 +11,14 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		fm := filemanager.New("pricessss.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmd := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		priceJob.Process()
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("Could not process input")
+			fmt.Println(err)
+		}
 	}
 }
